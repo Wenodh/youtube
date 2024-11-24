ab0338295c0cde5ca5e2a8b4fac302447459bac6
wenodh <vinodkumara3@gmail.com>
2024-11-24 17:27:25 +0530

    added load more to reduce api calls

--- src/components/body/VideoContainer.js
+++ src/components/body/VideoContainer.js
@@ -9,9 +9,10 @@ import { Link } from "react-router-dom";
 
 const VideoContainer = () => {
   const [videos, setVideos] = useState([]);
+  const [nextPageToken, setNextPageToken] = useState("");
   const [searchParams] = useSearchParams();
   const query = searchParams.get("q");
-  const categoryId = searchParams.get("categoryId")
+  const categoryId = searchParams.get("categoryId");
 
   useEffect(() => {
     getVideos();
@@ -26,22 +27,39 @@ const VideoContainer = () => {
       if (categoryId) {
         apiUrl += `&videoCategoryId=${categoryId}`;
       }
+      if (nextPageToken) {
+        apiUrl += `&pageToken=${nextPageToken}`;
+      }
       const res = await fetch(apiUrl);
       const data = await res.json();
-      setVideos(data.items);
+      setNextPageToken(data.nextPageToken);
+      setVideos([...videos, ...data.items]);
     } catch (err) {
       console.error("Failed to fetch videos", err);
     }
   };
 
   return (
-    <div className="grid grid-cols-1 gap-3 p-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {videos?.map((video) => (
-        <Link key={video.id?.videoId || video.id} to={"/watch?v=" + `${video?.id?.videoId || video.id}`}>
-          <VideoCard id={video.id?.videoId || video.id} info={video} />
-        </Link>
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-1 gap-3 p-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {videos?.map((video) => (
+          <Link
+            key={video.id?.videoId || video.id}
+            to={"/watch?v=" + `${video?.id?.videoId || video.id}`}
+          >
+            <VideoCard id={video.id?.videoId || video.id} info={video} />
+          </Link>
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <button
+          className="mx-auto my-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={getVideos}
+        >
+          Load more
+        </button>
+      </div>
+    </>
   );
 };
 
